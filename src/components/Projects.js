c19e7ef8020c5a51b24c629bcf24ef33b8845ac0
jl <jacoblepler91@gmail.com>
2020-12-08 13:30:39 -0500

    hover details effect on individual projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -134,30 +134,41 @@ const Projects = () => {
   const mouseOver = ({ target }) => {
     if (target.parentNode.nodeName.toLowerCase() === 'a') {
       target.className = 'hide'
+      target.parentNode.childNodes[1].className = 'details'
     }
     return
   }
 
-  const mouseLeave = ({ target}) => {
-    if (target.nodeName.toLowerCase() === 'a') {
-      target.childNodes[0].className = ''
+  const mouseLeave = ({ target }) => {
+    if (target.parentNode.parentNode.nodeName.toLowerCase() === 'a') {
+      target.parentNode.parentNode.childNodes[0].className = ''
+      target.parentNode.className = 'details hide'
+    } else if (
+      target.parentNode.parentNode.parentNode.nodeName.toLowerCase() === 'a'
+    ) {
+      console.log(target.parentNode.parentNode)
+      target.parentNode.parentNode.className = 'details hide'
+      target.parentNode.parentNode.parentNode.childNodes[0].className = ''
+    } else if (target.nodeName === 'a'){
+        target.childNodes[0].className = ''
+        target.childNodes[1].className = 'details hide'
     }
     return
   }
 
-  //   const details = () => {
-  //     return (
-  //       <div>
-  //         <h3>Title of Project</h3>
-  //         <p>Technologies Used</p>
-  //         <ul>
-  //           <li>React</li>
-  //           <li>Javascript</li>
-  //           <li>HTML</li>
-  //         </ul>
-  //       </div>
-  //     )
-  //   }
+  const details = (project) => {
+    return (
+      <div className="details hide">
+        <h3>{project.title}</h3>
+        <p>Techs Used:</p>
+        <ul className='project-techs'>
+          {project.technologies.map(tech => {
+              return <li>{tech} </li>
+          })}
+        </ul>
+      </div>
+    )
+  }
 
   return (
     <div className="projects container">
@@ -165,7 +176,7 @@ const Projects = () => {
       <ul className="project list">
         {showProjectsArray.map((project, index) => {
           return (
-            <li key={project.id}>
+            <li key={project.id} className='projects'>
               <a
                 href={project.link}
                 target="_blank"
@@ -174,6 +185,7 @@ const Projects = () => {
                 onMouseLeave={mouseLeave}
               >
                 <img src={project.image} alt="project-1" id="project-1" />
+                {details(project)}
               </a>
             </li>
           )
